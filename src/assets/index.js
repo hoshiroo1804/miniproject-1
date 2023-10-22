dbc42dc7f71f40ae00b16ac19ca7704b4dba15f5
hoshiroo <108223413+hoshiroo1804@users.noreply.github.com>
2023-10-22 22:46:28 +0700

    ya

---
+++ src/assets/index.js
@@ -0,0 +1,102 @@
+import caffe1 from './gambar/caffe/gambar1.jpeg';
+import caffe2 from './gambar/caffe/gambar2.jpeg';
+import caffe3 from './gambar/caffe/gambar3.jpeg';
+import caffe4 from './gambar/caffe/gambar4.jpeg';
+import caffe5 from './gambar/caffe/gambar5.jpeg';
+import caffe6 from './gambar/caffe/kopi1.jpeg';
+import caffe7 from './gambar/caffe/kopi2.jpeg';
+import caffe8 from './gambar/caffe/kopi3.jpeg';
+import caffe9 from './gambar/caffe/kopi4.jpeg';
+import caffe10 from './gambar/caffe/kopi5.jpeg';
+import caffe11 from './gambar/caffe/kopi6.jpeg';
+import caffe12 from './gambar/caffe/kopi7.jpeg';
+import caffe13 from './gambar/caffe/kopi8.jpeg';
+import caffe14 from './gambar/caffe/kopi9.jpeg';
+import caffe15 from './gambar/caffe/kopi10.jpeg';
+import caffe16 from './gambar/caffe/kopi11.jpeg';
+import caffe17 from './gambar/caffe/kopi12.jpeg';
+import caffe18 from './gambar/caffe/kopi13.jpeg';
+import caffe19 from './gambar/caffe/kopi14.jpeg';
+import caffe20 from './gambar/caffe/kopi15.jpeg';
+import caffe21 from './gambar/caffe/kopi16.jpeg';
+import caffe22 from './gambar/caffe/kopi17.jpeg';
+import caffe23 from './gambar/caffe/kopi18.jpeg';
+import caffe24 from './gambar/caffe/kop19.jpeg';
+import fresh1 from './gambar/fresh/fres.jpeg';
+import fresh2 from './gambar/fresh/fres1.jpeg';
+import fresh3 from './gambar/fresh/fres2.jpeg';
+import fresh4 from './gambar/fresh/fres3.jpeg';
+import fresh5 from './gambar/fresh/fres4.jpeg';
+import fresh6 from './gambar/fresh/fres5.jpeg';
+import fresh7 from './gambar/fresh/fres6.jpeg';
+import fresh8 from './gambar/fresh/fres7.jpeg';
+import fresh9 from './gambar/fresh/fres8.jpeg';
+import fresh10 from './gambar/fresh/fres9.jpeg';
+import fresh11 from './gambar/fresh/fres10.jpeg';
+import fresh12 from './gambar/fresh/fres11.jpeg';
+import fresh13 from './gambar/fresh/fres12.jpeg';
+import fresh14 from './gambar/fresh/fres13.jpeg';
+import fresh15 from './gambar/fresh/fres14.jpeg';
+import fresh16 from './gambar/fresh/fres15.jpeg';
+import fresh17 from './gambar/fresh/fres16.jpeg';
+import fresh18 from './gambar/fresh/fres17.jpeg';
+import fresh19 from './gambar/fresh/fres18.jpeg';
+import fresh20 from './gambar/fresh/fres19.jpeg';
+import fresh21 from './gambar/fresh/fres20.jpeg';
+import fresh22 from './gambar/fresh/fres21.jpeg';
+import icon1 from './gambar/icon/gambartoko.jpeg'
+import icon2 from './gambar/icon/kodeQR.jpeg';
+import icon3 from './gambar/icon/kopi.png';
+
+
+export { 
+    caffe1,
+    caffe2,
+    caffe3,
+    caffe4,
+    caffe5,
+    caffe6,
+    caffe7,
+    caffe8,
+    caffe9,
+    caffe10,
+    caffe11,
+    caffe12,
+    caffe13,
+    caffe14,
+    caffe15,
+    caffe16,
+    caffe17,
+    caffe18,
+    caffe19,
+    caffe20,
+    caffe21,
+    caffe22,
+    caffe23,
+    caffe24,
+    fresh1,
+    fresh2,
+    fresh3,
+    fresh4,
+    fresh5,
+    fresh6,
+    fresh7,
+    fresh8,
+    fresh9,
+    fresh10,
+    fresh11,
+    fresh12,
+    fresh13,
+    fresh14,
+    fresh15,
+    fresh16,
+    fresh17,
+    fresh18,
+    fresh19,
+    fresh20,
+    fresh21,
+    fresh22,
+    icon1,
+    icon2,
+    icon3,
+}
